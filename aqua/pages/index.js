bc1fb079344a9133602309a51f0f505d3236569f
ginanjartg <ginanjar.10119032@mahasiswa.unikom.ac.id>
2021-11-29 20:10:23 +0700

    nyoba
    oke

--- aqua/pages/index.js
+++ aqua/pages/index.js
@@ -5,6 +5,10 @@ export default function Home() {
     <body className="bg-gray-500">
       <h1 className="text-white">HALO BANG</h1>
       <div className="m-auto">TESSSSS</div>
+      <h1 className="text-white">HALO BANG</h1>
+      <div className="m-auto">TESSSSS</div>
+      <h1 className="text-white">HALO BANG</h1>
+      <div className="m-auto">TESSSSS</div>
     </body>
     </>
   )
