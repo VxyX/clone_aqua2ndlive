98e90df49eae109d0379137bcf8eab8a381aba7c
ginanjartg <ginanjar.10119032@mahasiswa.unikom.ac.id>
2021-12-01 22:13:08 +0700

    koding tampilan loading  + main
    lanjutin bg

--- aqua/pages/index.js
+++ aqua/pages/index.js
@@ -1,15 +1,31 @@
+import Head from "next/head";
+import Image from "next/image";
+import utama from "../public/images/svg/logo_2line_full.svg";
+import aqua from "../public/images/aqua_neko_1000-min.png";
 
 export default function Home() {
   return (
     <>
-    <body className="bg-gray-500">
-      <h1 className="text-white">HALO BANG</h1>
-      <div className="m-auto">TESSSSS</div>
-      <h1 className="text-white">HALO BANG</h1>
-      <div className="m-auto">TESSSSS</div>
-      <h1 className="text-white">HALO BANG</h1>
-      <div className="m-auto">TESSSSS</div>
-    </body>
+      <Head>
+        <title>Aqua Minato One-man Live 2022</title>
+        <meta name="description" content="Live Aqua 2022!" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+
+      <div className="h-screen">
+        <div className="flex-wrap bg-cross hidden">
+          <div className="place-content-center flex flex-wrap h-screen m-auto">
+            <Image src={utama} alt="Gambar Utama" />
+          </div>
+        </div>
+        <div className="bg-ungu h-auto flex flex-wrap place-content-center m-auto py-44">
+          <div id="beranda" className="bg-gedung animate-fade-in-dikit -my-10 w-3/4">
+            <div className="animate-fade-in-banyak -my-24">
+              <Image src={aqua} alt="Gambar Utama" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- aqua/tailwind.config.js
+++ aqua/tailwind.config.js
@@ -1,11 +1,83 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      hp: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
+
+    extend: {
+      keyframes: {
+        "fade-in-dikit": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-20px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in-banyak": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-150px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-down": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-dikit": "fade-in-dikit 0.3s ease-out",
+        "fade-in-banyak": "fade-in-banyak 2s ease-out",
+        "fade-out-down": "fade-out-down 1s ease-out",
+        "fade-in-up": "fade-in-up 1s ease-out",
+        "fade-out-up": "fade-out-up 1s ease-out",
+      },
+
+      backgroundImage: {
+        cross: "url('../public/images/texture_cross_dot.png')",
+        ungu: "url('../public/images/main_bg.jpg')",
+        gedung: "url('../public/images/kv_bg_ef_1920-min.jpg')",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
