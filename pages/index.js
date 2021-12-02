c2903d19a4ababa7b898d0b4dae40b772decccc7
ginanjartg <ginanjar.10119032@mahasiswa.unikom.ac.id>
2021-12-02 16:53:04 +0700

    disuruh sepuh bikin gambarnya ngikut width
    klh

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import utama from "../public/images/svg/logo_2line_full.svg";
 import aqua from "../public/images/aqua_neko_1000-min.png";
+import gedung from "../public/images/kv_bg_ef_1920-min.jpg";
 
 export default function Home() {
   return (
@@ -18,10 +19,18 @@ export default function Home() {
             <Image src={utama} alt="Gambar Utama" />
           </div>
         </div>
-        <div id="beranda" className="bg-ungu h-screen hp:h-auto flex flex-wrap place-content-center hp:py-44">
-          <div className="bg-gedung animate-fade-in-dikit m-auto hp:-my-10 h-1/2 hp:h-auto w-5/6 hp:w-5/6">
-            <div className="animate-fade-in-banyak hp:-my-24 hp:mx-36">
-              <Image src={aqua} alt="Gambar Utama" />
+        <div
+          id="beranda"
+          className="bg-ungu h-screen hp:h-auto grid place-content-center"
+        >
+          <div className="relative">
+            <div className="animate-fade-in-dikit m-auto h-auto py-10 px-10">
+              <div className="absolute">
+                <Image src={gedung} alt="Gambar Utama" />
+              </div>
+              <div className="animate-fade-in-banyak z-10">
+                <Image src={aqua} alt="Gambar Utama" />
+              </div>
             </div>
           </div>
         </div>
