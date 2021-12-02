0df18aa921560dd0df0fa791509026811266855d
M-Farhan-R <farhan.10119023@mahasiswa.unikom.ac.id>
2021-12-02 19:51:51 +0700

    awhdashdawf
    ntahlah

--- aqua/pages/index.js
+++ aqua/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import utama from "../public/images/svg/logo_2line_full.svg";
 import aqua from "../public/images/aqua_neko_1000-min.png";
+import aqua2 from "../public/images/aqua_neko_1500_pc-min.png";
 import gedung from "../public/images/kv_bg_ef_1920-min.jpg";
 
 export default function Home() {
@@ -12,26 +13,45 @@ export default function Home() {
         <meta name="description" content="Live Aqua 2022!" />
         <link rel="icon" href="favicon.ico" />
       </Head>
-
-      <div className="h-screen">
-        <div className="flex-wrap bg-cross hidden">
+      <div className="bg-ungu bg-no-repeat h-screen w-full fixed" >
+        {/* <div className="flex-wrap bg-cross">
           <div className="place-content-center flex flex-wrap h-screen m-auto">
             <Image src={utama} alt="Gambar Utama" />
           </div>
-        </div>
+        </div> */}
 
-        <div className="bg-ungu h-auto flex flex-wrap place-content-center m-0.5 py-16">
-          <div id="container" className="w-5/6 flex overflow-hidden">
-            <div id="beranda" className="animate-gedung-fade-in relative scale-y-full">
+        {/* <div className="h-auto block place-content-center px-14 py-20 box-border">
+          <div id="container" className="flex overflow-hidden">
+            <div id="beranda" className="animate-gedung-fade-in relative w-full h-auto">
               <Image src={gedung} />
-              
             </div>
-            <div className="animate-aqua-fade-in transform translate-x-dikit absolute h-full -my-16">
-                <Image src={aqua} alt="Gambar Utama" />
+            <div className="block absolute">
+              <div className="animate-aqua2-fade-in w-5/6 overflow-hidden">
+                <Image src={aqua2} alt="Gambar Utama" />
               </div>
+            </div>
           </div>
+        </div> */}
+
+      </div>
+      <div className="pt-24 pb-20 block overflow-hidden">
+        <div className="px-20 relative">
+          <div className="block relative">
+            <div className="w-full h-auto">
+              <Image src={gedung} className="animate-gedung-fade-in absolute"></Image>
+            </div>
+            <div className="animate-aqua2-fade-in transform -translate-x-1/2 -translate-y-1/2 left-tl top-tl1 w-aqua h-auto absolute block">
+              <Image src={aqua2} className=""></Image>
+            </div>
+          </div>
+
+        </div>
+        <div>
+          <Image src={utama} alt="Gambar Utama" /> //cuma test animasi bini sy 
         </div>
       </div>
+
+
     </>
   )
 }
--- aqua/tailwind.config.js
+++ aqua/tailwind.config.js
@@ -64,11 +64,21 @@ module.exports = {
         "aqua-fade-in": {
           "0%": {
             opacity: "0",
-            transform: "scale(1.7) translateX(15%) translateY(-10%)",
+            transform: "scale(2) translateY(-10%) translateX(15%)",
           },
           "100%": {
             opacity: "1",
-            transform: "scale(1) translateX(15%) translateY(0)",
+            transform: "scale(1) translateY(0) translateX(15%)",
+          }
+        },
+        "aqua2-fade-in": {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-50%, -50%) scale(2)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%, -50%) scale(1)",
           }
         },
         "gedung-fade-in": {
@@ -89,6 +99,7 @@ module.exports = {
         "fade-in-up": "fade-in-up 1s ease-out",
         "fade-out-up": "fade-out-up 1s ease-out",
         "aqua-fade-in": "aqua-fade-in 1.5s cubic-bezier(0.30, 1, 0.7, 1)",
+        "aqua2-fade-in": "aqua2-fade-in 1.5s cubic-bezier(0.30, 1, 0.7, 1)",
         "gedung-fade-in": "gedung-fade-in 1.5s cubic-bezier(0.30, 1, 0.7, 1)",
       },
 
@@ -98,14 +109,18 @@ module.exports = {
         gedung: "url('../public/images/kv_bg_ef_1920-min.jpg')",
       },
 
-      translate: {
-        dikit: '15%',
-      },
-
       scale: {
         full: '100%',
-      }
+      },
       
+      inset: {
+        'tl' : '58%',
+        'tl1': '53%',
+      },
+
+      spacing: {
+        aqua: '89%',
+      }
     },
   },
   variants: {
