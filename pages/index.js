f59d989b3c9183fc2fb638dfd097003c8ff10950
ginanjartg <ginanjar.10119032@mahasiswa.unikom.ac.id>
2021-12-02 17:10:00 +0700

    benerin lg

--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,11 @@ export default function Home() {
           className="bg-ungu h-screen hp:h-auto grid place-content-center"
         >
           <div className="relative">
-            <div className="animate-fade-in-dikit m-auto h-auto py-10 px-10">
-              <div className="absolute">
+            <div className="animate-fade-in-dikit m-auto h-auto">
+              <div className="absolute p-5 hp:p-10">
                 <Image src={gedung} alt="Gambar Utama" />
               </div>
-              <div className="animate-fade-in-banyak z-10">
+              <div className="animate-fade-in-banyak">
                 <Image src={aqua} alt="Gambar Utama" />
               </div>
             </div>
