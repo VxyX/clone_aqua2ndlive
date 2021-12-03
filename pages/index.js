c4b77059ceb93f5e6d0fd19d85918851346274a5
M-Farhan-R <farhan.10119023@mahasiswa.unikom.ac.id>
2021-12-03 11:14:01 +0700

    qeomnfrjiawenb gfviwas

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,9 @@ export default function Home() {
           <div className="place-content-center flex flex-wrap h-screen m-auto">
             <Image src={utama} alt="Gambar Utama" />
           </div>
-        </div> */}
-
-        {/* <div className="h-auto block place-content-center px-14 py-20 box-border">
+        </div> 
+        
+        <div className="h-auto block place-content-center px-14 py-20 box-border">
           <div id="container" className="flex overflow-hidden">
             <div id="beranda" className="animate-gedung-fade-in relative w-full h-auto">
               <Image src={gedung} />
