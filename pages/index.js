b8046be7ad85221654bfc60e732d5daa9537620f
M-Farhan-R <farhan.10119023@mahasiswa.unikom.ac.id>
2021-12-03 11:06:29 +0700

    ..

--- pages/index.js
+++ pages/index.js
@@ -12,25 +12,38 @@ export default function Home() {
         <meta name="description" content="Live Aqua 2022!" />
         <link rel="icon" href="favicon.ico" />
       </Head>
-
-      <div className="h-screen">
-        <div className="flex-wrap bg-cross hidden">
+      <div className="bg-ungu bg-no-repeat h-screen w-full fixed" >
+        {/* <div className="flex-wrap bg-cross">
           <div className="place-content-center flex flex-wrap h-screen m-auto">
             <Image src={utama} alt="Gambar Utama" />
           </div>
-        </div> knf
+        </div> */}
 
-        <div className="h-auto block place-content-center px-14 py-20 box-border">
+        {/* <div className="h-auto block place-content-center px-14 py-20 box-border">
           <div id="container" className="flex overflow-hidden">
             <div id="beranda" className="animate-gedung-fade-in relative w-full h-auto">
               <Image src={gedung} />
-              
             </div>
-            <div className="animate-aqua-fade-in transform translate-x-dikit absolute h-full -my-16">
-                <Image src={aqua} alt="Gambar Utama" />
+            <div className="block absolute">
+              <div className="animate-aqua2-fade-in w-5/6 overflow-hidden">
+                <Image src={aqua2} alt="Gambar Utama" />
               </div>
+            </div>
           </div>
- 
+        </div> */}
+
+      </div>
+      <div className="pt-24 pb-20 block overflow-hidden">
+        <div className="px-20 relative">
+          <div className="block relative">
+            <div className="w-full h-auto">
+              <Image src={gedung} className="animate-gedung-fade-in absolute"></Image>
+            </div>
+            <div className="animate-aqua2-fade-in transform -translate-x-1/2 -translate-y-1/2 left-tl top-tl1 w-aqua h-auto absolute block">
+              <Image src={aqua2} className=""></Image>
+            </div>
+          </div>
+
         </div>
         <div>
           <Image src={utama} alt="Gambar Utama" /> {/*cuma test animasi bini sy */}
@@ -49,11 +62,11 @@ export default function Home() {
           className="bg-ungu h-screen hp:h-auto grid place-content-center"
         >
           <div className="relative">
-            <div className="animate-fade-in-dikit m-auto h-auto py-10 px-10">
-              <div className="absolute"> 
+            <div className="animate-fade-in-dikit m-auto h-auto">
+              <div className="absolute p-5 hp:p-10">
                 <Image src={gedung} alt="Gambar Utama" />
               </div>
-              <div className="animate-fade-in-banyak z-10">
+              <div className="animate-fade-in-banyak">
                 <Image src={aqua} alt="Gambar Utama" />
               </div>
             </div>
